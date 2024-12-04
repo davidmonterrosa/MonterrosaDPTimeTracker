c01303843d951d3e7f7f8299dd464f072eeaefe4
davidmonterrosa <dmonterrosa99@gmail.com>
2024-12-04 12:15:43 -0800

    app.js initial ID Section set up

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+// alert("JS Connected")
+
+// IDs Section
+let daily = document.getElementById("daily");
+let weekly = document.getElementById("weekly");
+let monthly = document.getElementById("monthly");
+
+let workPrevious = document.getElementById("workPrevious");
+let playPrevious = document.getElementById("playPrevious");
+let studyPrevious = document.getElementById("studyPrevious");
+let exercisePrevious = document.getElementById("exercisePrevious");
+let socialPrevious = document.getElementById("socialPrevious");
+let selfCarePrevious = document.getElementById("selfCarePrevious");
+
+let workDisplay = document.getElementById("workDisplay");
+let playDisplay = document.getElementById("playDisplay");
+let studyDisplay = document.getElementById("studyDisplay");
+let exerciseDisplay = document.getElementById("exerciseDisplay");
+let socialDisplay = document.getElementById("socialDisplay");
+let selfCareDisplay = document.getElementById("selfCareDisplay");
+
+// Data Call
+// daily.addEventListener('click', function(){
+
+// })
